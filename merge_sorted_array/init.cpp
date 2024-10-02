62b30c6107072ec6ae390a4167a1a6f596dd7bd6
bat <acc.diceroll@gmail.com>
2024-10-02 13:29:36 +0200

    Update init.cpp

--- merge_sorted_array/init.cpp
+++ merge_sorted_array/init.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged.
+The last n elements are set to 0 and should be ignored. nums2 has a length of n.
+*/
+
 class Solution {
 public:
   void merge (vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int size {m + n};
+    int const size {m + n};
     int arr[size];
 
     for (int i = 0; i < m; i++) {
